4d192ba8ac33f1f5659ce300ac7ba85cb31a2d92
Aman pandey <152058135+Siddhant7300@users.noreply.github.com>
2025-05-21 12:51:04 +0530

    Update script.js

--- Front End/assets/js/script.js
+++ Front End/assets/js/script.js
@@ -86,7 +86,7 @@ document.getElementById("contact-form").addEventListener("submit", function(even
       message: formData.get('message')
     };
     console.log("Form data:", data);
-    alert(data);
+    alert(data.name);
 
     // Send the data to the backend using fetch
     fetch("https://brilliantsads.onrender.com/contact", {
